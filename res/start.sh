fab3e18b44b70b270fb62509674265563f05535d
Chris <pfcoder97@gmail.com>
2024-09-18 02:19:34 +0000

    update

--- res/start.sh
+++ res/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-nohup /opt/omni-agent/lcd-agent  > /var/log/omni-agent.log 2>&1 &
+cd /opt/omni-agent/
+nohup ./lcd-agent  > /var/log/omni-agent.log 2>&1 &
 echo $! > /opt/omni-agent/pid
